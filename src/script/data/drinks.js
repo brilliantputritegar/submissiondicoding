a3fec98291b562558a1693d27a31dd98d07f1fea
brilliantputritegar <brilliantputritegar@gmail.com>
2022-10-28 01:04:22 +0700

    submission

---
+++ src/script/data/drinks.js
@@ -0,0 +1,59 @@
+const drinks = [
+  {
+    name: "GG",
+    stadium: "Ordinary Drink",
+    image:
+      "https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg",
+    instructions:
+      "Pour the Galliano liqueur over ice. Fill the remainder of the glass with ginger ale and thats all there is to it. You now have a your very own GG.",
+  },
+  {
+    name: "A1",
+    stadium: "Cocktail",
+    image:
+      "https:/www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg",
+    instructions:
+      "Pour all ingredients into a cocktail shaker, mix and serve over ice into a chilled glass.",
+  },
+  {
+    name: "ABC",
+    stadium: "Shot",
+    image:
+      "https:/www.thecocktaildb.com/images/media/drink/tqpvqp1472668328.jpg",
+    instructions: "Layered in a shot glass.",
+  },
+  {
+    name: "kir",
+    stadium: "Ordinary Drink",
+    image:
+      "https:/www.thecocktaildb.com/images/media/drink/apneom1504370294.jpg",
+    instructions:
+      "Add the cr/u00e8me de cassis to the bottom of the glass, then top up with wine.",
+  },
+  {
+    name: "747",
+    stadium: "Shot",
+    image:
+      "https:/www.thecocktaildb.com/images/media/drink/xxsxqy1472668106.jpg",
+    instructions:
+      "pour kaluha, then Baileys, then Frangelico not chilled and not layered -- SERVE!!!.",
+  },
+  {
+    name: "252",
+    stadium: "Shot",
+    image:
+      "https:/www.thecocktaildb.com/images/media/drink/rtpxqw1468877562.jpg",
+    instructions:
+      "Add both ingredients to shot glass, shoot, and get drunk quick.",
+  },
+  {
+    name: "Ace",
+    stadium: "Cocktail",
+    image:
+      "https:/www.thecocktaildb.com/images/media/drink/l3cd7f1504818306.jpg",
+    instructions:
+      "Shake all the ingredients in a cocktail shaker and ice then strain in a cold glass.",
+  },
+];
+
+export default drinks;
